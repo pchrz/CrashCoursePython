312d57ff7a58aa22e31687948730b9a030ba61d8
pchrz <petchar@ut.utm.edu>
2021-03-14 16:38:48 +0000

    End of week1

---
+++ numbers.py
@@ -0,0 +1,18 @@
+#Python working with numbers
+
+print(4+5)
+
+print(9*7)
+
+print(-1/4) 
+
+print(1/3)
+
+print (((2050/5)-32)/9)
+
+#Two to the power of 10
+print(2**10)
+
+#My answers don't show decimal points like they do  in the course videos, why? 
+
+
---
+++ variables.py
@@ -0,0 +1,5 @@
+# seperate the print call from the elements it's printing for more generic output. 
+
+color = "Blue"
+thing = "leash"
+print(color + " is the color of " + thing )
